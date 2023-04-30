7c1231d1437c41ab1a4dff35d308da79c6a589a6
María Lynne Capa <malynne@lg-tv.local>
2023-04-30 17:36:52 -0600

    Principal added

--- src/main/java/com/driveai/documentsms/controllers/DocumentController.java
+++ src/main/java/com/driveai/documentsms/controllers/DocumentController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("/v1/document")
 public class DocumentController {
