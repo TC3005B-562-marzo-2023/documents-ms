c0f10f8a781fdb0a5872e8f0478a06069fbbfd7a
CarlaOnate <carlaonategardella@gmail.com>
2023-05-01 18:45:59 -0600

    Created DocumentRequired create endpoint, added controller, service and modified created_at in model.

--- src/main/java/com/driveai/documentsms/services/DocumentRequiredService.java
+++ src/main/java/com/driveai/documentsms/services/DocumentRequiredService.java
@@ -4,6 +4,7 @@ import com.driveai.documentsms.dto.DocumentRequiredDto;
 import com.driveai.documentsms.models.DocumentRequired;
 import com.driveai.documentsms.repositories.DocumentRequiredRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,6 +14,13 @@ import java.util.List;
 public class DocumentRequiredService {
     @Autowired
     DocumentRequiredRepository documentRequiredRepository;
+
+    public DocumentRequired saveRequiredDocument(DocumentRequired documentRequired) {
+        if (documentRequired.getDocumentRequiredId() == 0) {
+            documentRequired = documentRequiredRepository.save(documentRequired);
+        }
+        return documentRequired;
+    }
 
     public List<DocumentRequiredDto> findAll() {
         List<DocumentRequired> documentRequiredList = documentRequiredRepository.findAll();
